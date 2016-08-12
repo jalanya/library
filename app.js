88b44ab9f3d6f9af49f33df8242dfc12a3c3c383
Jose Alanya <jose.alanya@gmail.com>
2016-08-11 19:42:25 -0500

    Simple routing

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.get('/books', function(req, res) {
+  res.send('Hello Books');
+});
+
 app.listen(port, function() {
    console.log('Running server on port ' + port);
 });
