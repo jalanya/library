d4c372d898bd0e0511ef94bfaa25d653f8a8f344
Jose Alanya <jose.alanya@gmail.com>
2016-08-11 19:31:06 -0500

    Run Libray Web app on a port

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var express = require('express')
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.listen(port, function() {
+   console.log('Running server on port ' + port);
+});
